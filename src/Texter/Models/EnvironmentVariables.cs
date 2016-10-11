de61ea5179d5fe45e1b231936775defd809b5341
Joel <joeltwaage@gmail.com>
2016-10-11 11:05:01 -0700

    Create Text Message App

---
+++ src/Texter/Models/EnvironmentVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Texter.Models
+{
+    public class EnvironmentVariables
+    {
+        public static string AccountSid = "ACa2c31d7210c8e998dec91c784b4e922e";
+        public static string AuthToken = "348a8cd48582b1a8289e1bc3db41cd08";
+    }
+}
